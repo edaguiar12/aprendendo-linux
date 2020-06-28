3adf92576776191f9d2f54b6e78306dfdcdd1345
Ederson Rafael Silva de Aguiar <edersonrafael12@gmail.com>
2020-06-28 14:33:22 -0300

    Refactoring relatoriodosprocessos.sh
    -execution improvements;
    -total translation to portuguese;

--- relatoriodosprocessos.sh
+++ relatoriodosprocessos.sh
@@ -1,43 +1,33 @@
 #!/usr/bin/bash
 
-function pause(){
+function pause() {
     read -p "$*"
 }
-
-if [ -d != "/Documents/Processos" ];
-then
-    echo "removing trash along the way.."
-    rm -rf Processos
-fi
 
 echo -e "Vamos criar um arquivo de informações dos processos? \nSim: digite y \nNão: digite n"
 
 read yesno
 
-if [ $yesno != "y" ];
-then
+if [ $yesno != "y" ]; then
     exit 1
 else
+
+    if [ -d != "/Documents/Processos" ]; then
+        echo "Vamos começar removendo aquivos e pastas criados anteriormente..."
+        rm -rf Processos
+    fi
     echo "criando a pasta processos e seu subdiretório.."
     cd ~/Documents && mkdir Processos && cd Processos && mkdir Processos2 && cd Processos2
-    pause 'Pressione o botão [Enter] para continuar...'
     echo "criando o arquivo proc.."
     touch proc.txt
-    pause 'Pressione o botão [Enter] para continuar...'
     echo "colocando os processos dentro do arquivo.."
     df -la | cat > proc.txt
-    pause 'Pressione o botão [Enter] para continuar...'
-    echo "Vamos visualizar o arquivo criado?"
-    less proc.txt
-    pause 'Pressione o botão [Enter] para continuar...'
-    echo "Vamos agora fazer um backup de nosso arquivo!"
+    echo "criando um backup de nosso arquivo"
     cp proc.txt procBkp.txt && mv procBkp.txt ~/Documents/Processos
-    pause 'Pressione o botão [Enter] para continuar...'
-    echo "Vamos então excluir o arquivo e a pasta que criamos!"
+    echo "Excluindo os arquivos e pastas que não iremos mais usar.."
     cd ~/Documents/Processos && rm -rf Processos2
-    pause 'Pressione o botão [Enter] para continuar...'
     echo "Pronto!"
+    pause "Aperte uma tecla para visualizar o arquivo criado. 
+    Use as setas do teclado para percorrer o arquivo e quando tiver terminado aperte a tecla (q) do teclado!"
+    less procBkp.txt
 fi
-
-
-
